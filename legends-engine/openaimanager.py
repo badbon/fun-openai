f77eb70fe77a6cea480641485eb5cfabc8551dbb
Nick <ndplay@ymail.com>
2023-04-18 17:05:35 +0400

    Fixing year generation to be personal, not bound to groups
    Fixing year generation to be personal, not bound to groups

--- legends-engine/openaimanager.py
+++ legends-engine/openaimanager.py
@@ -6,18 +6,6 @@ import string
 openai.api_key = "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"
 model_engine = "text-davinci-002"
 
-# Generate basic physical facts and personal motivations
-basic_prompt = "Create a fantasy character who lives in a medieval world and explores dungeons to defeat skeletons and other monsters! Describe the character's race, hair color and style, and personal motivations."
-basic_response = openai.Completion.create(
-    engine=model_engine,
-    prompt=basic_prompt,
-    max_tokens=200,
-    n=1,
-    stop=None,
-    temperature=0.5,
-    timeout=20,  
-)
-
 def generate_name():
     """Generate a random fantasy character name."""
     first_names = ["Aelar", "Aerin", "Aeron", "Aiden", "Aila", "Ailis", "Aislinn", "Alaric", "Aldair", "Alden", "Althaea", "Althea", "Amaranth", "Amaryllis", "Amethyst", "Anastaria", "Andriel", "Aneira", "Aneirin", "Arael", "Araminta", "Arcadia", "Arcturus", "Ariadne", "Arianrhod", "Arien", "Arin", "Arista", "Aristaeus", "Aristeia", "Artemis", "Arya", "Asphodel", "Astrid", "Atalanta", "Atara", "Aurora", "Avalon", "Aviana", "Axel", "Ayla", "Azura"]
@@ -47,28 +35,29 @@ def generate_word(length):
    word = ''.join(random.choice(letters) for _ in range(length))
    return word
 
-# Define OpenAI prompt
-history_prompt = f"Create a {race.lower()} character with {hair_color} {hair_style} hair. Describe the character's personality and motivations. What adventures has the character been on in their quest for {motivation}? Generate a short story about the character."
 
-history_response = openai.Completion.create(
+# Generate basic physical facts and personal motivations
+basic_prompt = "Create a fantasy RPG character who lives in a medieval world and explores dungeons to defeat skeletons and other monsters! Do not actually describe their equipment or skills, just motivations and personality"
+basic_response = openai.Completion.create(
     engine=model_engine,
-    prompt=history_prompt,
-    max_tokens=200,
+    prompt=basic_prompt,
+    max_tokens=300,
     n=1,
     stop=None,
-    temperature=0.5,
-    timeout=20, 
+    temperature=1.0,
+    timeout=20,  
 )
-# Define OpenAI prompt
+
+# OpenAI prompt
 history_prompt = f"Create a medieval fantasy RPG {name.lower()} character with {hair_color} {hair_style} hair. of {race.lower()} race. Describe the character's personality and motivations. What adventures has the character been on in their quest for {motivation}? Generate a short story about the character."
 
 history_response = openai.Completion.create(
     engine=model_engine,
     prompt=history_prompt,
-    max_tokens=200,
+    max_tokens=300,
     n=1,
     stop=None,
-    temperature=0.5,
+    temperature=0.8,
     timeout=20, 
 )
 
@@ -76,7 +65,7 @@ history_response = openai.Completion.create(
 history = history_response.choices[0].text
 
 # Generate year-by-year history for selected intervals
-year_by_year = ""
+""" year_by_year = ""
 for i in range(4):
     year = random.randint(0, 500)
     year_prompt = f"{history}\n\nIn the year {year}, what happened to {race.lower()}?"
@@ -91,11 +80,14 @@ for i in range(4):
     )
     year_event = year_response.choices[0].text
     year_by_year += f"\n\nYear {year}:\n{year_event}"
-
+ """
 
 # Print results
-print(f"Race: {race}")
-print(f"Hair Color: {hair_color}")
-print(f"Hair Style: {hair_style}")
+print(basic_response.choices[0].text)
+print(history_response.choices[0].text)
+
+#print(f"Race: {race}")
+#print(f"Hair Color: {hair_color}")
+#print(f"Hair Style: {hair_style}")
 print(f"Motivations: {', '.join(motivations)}")
-print(f"\nYear-by-Year History:\n{year_by_year}")
+#print(f"\nYear-by-Year History:{year_by_year}")
