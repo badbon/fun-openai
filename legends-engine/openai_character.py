58ea1dc21ec15e2951d83f689d12e70ab6f05d2d
Nick <ndplay@ymail.com>
2023-05-01 01:37:25 +0400

    Streamlining together modules

--- legends-engine/openai_character.py
+++ legends-engine/openai_character.py
@@ -15,67 +15,69 @@ def generate_name():
     last_name = random.choice(last_names)
     return f"{first_name} {last_name}"
 
-# Define possible options for character attributes
-races = ["Human", "Dwarf", "Elf"]
-hair_colors = ["black", "blonde", "brown", "red", "gray"]
-hair_styles = ["short", "medium", "long", "curly", "straight"]
-motivations = ["glory", "seeking life meaning", "riches", "power", "knowledge"]
+def create_character():
+    # Define possible options for character attributes
+    races = ["Human", "Dwarf", "Elf"]
+    hair_colors = ["black", "blonde", "brown", "red", "gray"]
+    hair_styles = ["short", "medium", "long", "curly", "straight"]
+    motivations = ["glory", "seeking life meaning", "riches", "power", "knowledge"]
 
-types_of_character_intent = ["trade and converse with adventurers! ", "find a new adventurer to lead him to battle and glory.", "defeat skeletons and other monsters!", "get very rich exploring and gathering ores and gems in dangerous dungeons" ]
-prompt_clarification = "Do not actually describe their equipment, or current age. this section is more of an intro and short biography. Character Name: "
+    types_of_character_intent = ["trade and converse with adventurers! ", "find a new adventurer to lead him to battle and glory.", "defeat skeletons and other monsters!", "get very rich exploring and gathering ores and gems in dangerous dungeons" ]
+    prompt_clarification = "Do not actually describe their equipment, or current age. this section is more of an intro and short biography. Character Name: "
 
-# Randomly select character attributes
-name = generate_name()
+    # Randomly select character attributes
+    name = generate_name()
 
-race = random.choice(races)
-hair_color = random.choice(hair_colors)
-hair_style = random.choice(hair_styles)
-motivation = random.choice(motivations)
-intent = random.choice(types_of_character_intent)
- 
-# More customized prompts testing
-custom_prompt = "Create a fantasy RPG character who lives in a medieval fictional world " + " and currently lives in the dungeons to " + intent + prompt_clarification + name.lower()
+    race = random.choice(races)
+    motivation = random.choice(motivations)
+    intent = random.choice(types_of_character_intent)
+    
+    # More customized prompts testing
+    custom_prompt = "Create a fantasy RPG character who lives in a medieval fictional world " + " and currently lives in the dungeons to " + intent + prompt_clarification + name.lower()
 
-# OpenAI prompt
-basic_response = openai.Completion.create(
-    engine=model_engine,
-    prompt=custom_prompt,
-    max_tokens=180,
-    n=1,
-    stop=None,
-    temperature=1.0,
-    timeout=20,  
-)
+    # OpenAI prompt
+    basic_response = openai.Completion.create(
+        engine=model_engine,
+        prompt=custom_prompt,
+        max_tokens=180,
+        n=1,
+        stop=None,
+        temperature=1.0,
+        timeout=20,  
+    )
 
-history_prompt = f"Create a medieval fantasy RPG {name} character with of {race.lower()} race. Describe the character's personality and motivations. What adventures has the character been on in their quest for {motivation}? \
-        Generate a short adventures story about the character. Be aware of preceeding intro not to make contradictions: " + basic_response.choices[0].text
-print("DEBUG: " + history_prompt)      
-history_response = openai.Completion.create(
-    engine=model_engine,
-    prompt=history_prompt,
-    max_tokens=240,
-    n=1,
-    stop=None,
-    temperature=1.0,
-    timeout=20, 
-)
+    history_prompt = f"Create a medieval fantasy RPG {name} character with of {race.lower()} race. Describe the character's personality and motivations. What adventures has the character been on in their quest for {motivation}? \
+            Generate a short adventures story about the character. Be aware of preceeding intro not to make contradictions: " + basic_response.choices[0].text
+    print("DEBUG: " + history_prompt)      
+    history_response = openai.Completion.create(
+        engine=model_engine,
+        prompt=history_prompt,
+        max_tokens=240,
+        n=1,
+        stop=None,
+        temperature=1.0,
+        timeout=20, 
+    )
 
-def debug():
-    print("Character name: " + char_name)
-    print("Character motivations: " + char_motivations)
-    print("Character intent: " + intent)
+    def debug():
+        print("Character name: " + char_name)
+        print("Character motivations: " + char_motivations)
+        print("Character intent: " + intent)
 
-# Extract information from history response
-history = history_response.choices[0].text
+    # Extract information from history response
+    history = history_response.choices[0].text
 
-# Extract character name and other necessary attributes
+    # Extract character name and other necessary attributes
 
-char_name = name
-char_motivations = motivation
+    char_name = name
+    char_motivations = motivation
 
-# Print final results
-debug()
+    # Print final results
+    debug()
 
-print(basic_response.choices[0].text)
-print("\nHISTORY RESPONSE:" + history_response.choices[0].text)
+    print(basic_response.choices[0].text)
+    print("\nHISTORY RESPONSE:" + history_response.choices[0].text)
+    
+    return history_response.choices[0].text, char_name, char_motivations
 
+create_character()
